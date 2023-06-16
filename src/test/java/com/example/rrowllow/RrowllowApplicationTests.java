d2bab098e18ae8dd757cc445b1d0aecfebbf7a19
gwkim <gwkim@softnet.co.kr>
2023-06-16 14:50:51 +0900

    Initial commit

---
+++ src/test/java/com/example/rrowllow/RrowllowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rrowllow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrowllowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
