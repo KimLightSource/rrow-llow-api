c1c02990a023bcb17b78141dabbfb68dd6571c57
gwkim <gwkim@softnet.co.kr>
2023-06-27 11:31:36 +0900

    BaseEntity 수정 및 적용

--- src/main/java/com/example/rrowllow/entity/BaseEntity.java
+++ src/main/java/com/example/rrowllow/entity/BaseEntity.java
@@ -1,17 +1,22 @@
 package com.example.rrowllow.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-@Data
-public class BaseEntity {
+@Getter
+public abstract class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createDate; //생성일
 
     @LastModifiedDate
